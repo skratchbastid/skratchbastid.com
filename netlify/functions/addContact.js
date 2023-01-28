c598830a9275a9362831111bf46e01454aea0c83
Greg Bates <gbatesy@gmail.com>
2023-01-28 14:24:07 -0400

    Testing mailjet function

--- netlify/functions/addContact.js
+++ netlify/functions/addContact.js
@@ -4,23 +4,8 @@ exports.handler = async function(event, context) {
     //     body: JSON.stringify({ message: "Hello Greg!" })
     // }
     const mailjet = require ('node-mailjet')
-    
-    mailjet.connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
-    const request = mailjet
-        .post("contact", {'version': 'v3'})
-        .request({
-        "IsExcludedFromCampaigns":"true",
-        "Name":"New Contact",
-        "Email":"passenger@mailjet.com"
-        })
-    request
-        .then((result) => {
-            return {
-                statusCode: 200,
-                body: result.body
-            }
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+    return {
+        statusCode: 200,
+        body: mailjet
+    }   
 }
