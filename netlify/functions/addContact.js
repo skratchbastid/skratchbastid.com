45580d1cb632a9befdace3de12fe2c0a06a64b6e
Greg Bates <gbatesy@gmail.com>
2023-01-28 14:43:30 -0400

    Testing mailjet function

--- netlify/functions/addContact.js
+++ netlify/functions/addContact.js
@@ -2,7 +2,7 @@ const mailjet = require('node-mailjet')
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello Greg! Let's figureo ut how to use Mailjet in here..." }),
-        mailjet: JSON.stringify(mailjet)
+        body: JSON.stringify({ message: "Hello Greg! Let's figureo ut how to use Mailjet in here...", mailjet: mailjet }),
+        mailjet: JSON.stringify(...mailjet)
     }   
 }
