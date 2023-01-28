c24a02c46778194e048e927397350111ed64126e
Greg Bates <gbatesy@gmail.com>
2023-01-28 15:02:55 -0400

    Testing mailjet function

--- netlify/functions/addContact.js
+++ netlify/functions/addContact.js
@@ -1,6 +1,16 @@
-const mailjet = require('node-mailjet')
+const Mailjet = require('node-mailjet');
 exports.handler = async function(event, context) {
-    console.log("Testing 123...")
+    const mailjet = new Mailjet({
+        apiKey: process.env.MJ_APIKEY_PUBLIC,
+        apiSecret: process.env.MJ_APIKEY_PRIVATE,
+    });
+    const request = mailjet
+        .post('contact')
+        .request({
+          Email: "passenger@mailjet.com",
+          IsExcludedFromCampaigns: true,
+          Name: "New Contact"
+        })
     return {
         statusCode: 200,
         body: JSON.stringify({ message: "Hello Greg! Let's figureo ut how to use Mailjet in here..."})
