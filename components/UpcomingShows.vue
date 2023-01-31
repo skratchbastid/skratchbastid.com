487458ff6dccd41cfa5f933b47f40a962e54b14b
Greg Bates <gbatesy@gmail.com>
2023-01-30 22:50:52 -0400

    Fix dates on show ticker

--- components/UpcomingShows.vue
+++ components/UpcomingShows.vue
@@ -10,10 +10,10 @@
     <NuxtLink to="/shows">
         <div class="relative flex py-4 md:py-6 overflow-x-hidden text-xl md:text-4xl bg-slate-900"  @mouseover="paused = true" @mouseleave="paused = false">
             <div class="text-white whitespace-nowrap animate-marquee flex" :style="paused ? 'animation-play-state: paused;' :''">
-                <div v-for="show in shows"><span class="mx-10 font-black uppercase">{{show.city}} <span class="font-thin">Dec 26, 2022</span></span></div>
+                <div v-for="show in shows"><span class="mx-10 font-black uppercase">{{show.city}} <span class="font-thin">{{ $dayjs(show.date).format('MMM D, YYYY')}}</span></span></div>
             </div>
             <div class="absolute animate-marquee2 whitespace-nowrap text-white flex" :style="paused ? 'animation-play-state: paused;' :''">
-                <div v-for="show in shows"><span class="mx-10 font-black uppercase">{{show.city}} <span class="font-thin">Dec 26, 2022</span></span></div>
+                <div v-for="show in shows"><span class="mx-10 font-black uppercase">{{show.city}} <span class="font-thin">{{ $dayjs(show.date).format('MMM D, YYYY')}}</span></span></div>
             </div>
         </div>
     </NuxtLink>
