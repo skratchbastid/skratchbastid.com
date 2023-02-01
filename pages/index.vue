bba73ed6aa63c1b8f36871a35c4f87d9e5d4ccb0
Greg Bates <gbatesy@gmail.com>
2023-02-01 12:35:00 -0400

    Update merch image

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
                     <div class="text-sm relative top-1">SHOP</div>
                     <div class="text-3xl font-black">MERCH</div>
                 </a>
-                <img src="/img/merch-items.png" alt="" class="w-4/5 md:w-[80%] -top-[40px]">
+                <img src="/img/sauce-apron.png" alt="" class="w-4/5 md:w-[80%] -top-[40px]">
             </div>
         </div>
         <SocialLinks />
