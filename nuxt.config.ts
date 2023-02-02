31e3f86b3d8ba56a6f2630d26b282cdabddba7d7
Greg Bates <gbatesy@gmail.com>
2023-02-02 17:36:42 -0400

    Meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default {
+    head: {
+        title: 'Skratch Bastid',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'The official home of Skratch Bastid. DJ. Producer. Someone to count on in a world ever-changing.'
+          }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
     modules: [
         '@nuxtjs/tailwindcss',
         'nuxt-icon',
