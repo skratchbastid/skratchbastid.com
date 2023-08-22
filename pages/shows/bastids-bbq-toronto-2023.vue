bfbb392648a5fa8b4544c6e3954917880da07360
Greg Bates <gbatesy@gmail.com>
2023-08-22 00:04:07 -0300

    debugging

--- pages/shows/bastids-bbq-toronto-2023.vue
+++ pages/shows/bastids-bbq-toronto-2023.vue
@@ -4,7 +4,9 @@
 
     const view = ref('recap')
     const photos = ref()
-    const { data } = await useFetch('https://api.publicapis.org/entries')
+    const { data } = await useFetch('/.netlify/functions/getImages')
+    photos.value = data.value.photos
+    console.log(photos.value)
 </script>
 
 <template>
@@ -48,9 +50,9 @@
                 {{ photos?.length }} photos<br />
                 wtf
                 <div class="grid grid-cols-2 md:grid-cols-4 md:grid-cols-3 gap-6">
-                    <div v-for="photo in data" class="aspect-4x3 rounded">
+                    <div v-for="photo in photos" class="aspect-4x3 rounded">
                         <!-- <nuxt-img provider="cloudinary" :src="photo" class="rounded" /> -->
-                        {{  photo }}
+                        <img :src="photo.url" class="rounded aspect-square object-cover" />
                     </div>
                 </div>
             </div>
