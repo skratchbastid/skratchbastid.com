e17978741497dd2cf004641114bca1a5e27c273b
Greg Bates <gbatesy@gmail.com>
2023-08-30 17:57:36 -0300

    Make recap banners on index page uniform

--- pages/bbq-recaps/index.vue
+++ pages/bbq-recaps/index.vue
@@ -8,10 +8,10 @@
             <div class="text-3xl font-black uppercase mb-4">Bastid's BBQ Recaps</div>
             <div class="text-lg mb-4">Relive the magic of Bastid's BBQ with these photo and video recaps.</div>
         </div>
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 px-4 md:px-8 lg:px-28 gap-x-2 md:gap-x-8 gap-y-8">
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 px-4 md:px-8 lg:px-28 gap-x-2 md:gap-x-8 gap-y-8 w-full">
             <NuxtLink :to="'/bbq-recaps/' + show.slug" v-for="show in recapStore.events" :key="show.date" class="event flex flex-col cursor-pointer">
-                <div class="max-h-30">
-                    <img :src="show.banner" alt="" class="object-fit max-h-30">
+                <div class="h-24 md:h-36 lg:h-48 bg-red-500 w-full">
+                    <img :src="show.banner" alt="" class="object-cover w-full h-full">
                 </div>
                 <div class="flex flex-col flex-grow justify-center gap-1 bg-white shadow-sm p-3">
                     <span class="text-lg font-bold">{{ show.city }}</span>
