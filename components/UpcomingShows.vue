08a0fc51c5bfcd338a578fa8a3dc361f40941539
Greg Bates <gbatesy@gmail.com>
2023-10-18 20:51:56 -0300

    Remove tilt from shows maruee on mobile

--- components/UpcomingShows.vue
+++ components/UpcomingShows.vue
@@ -10,7 +10,7 @@
 
 <template>
     <NuxtLink to="/shows">
-        <div v-if="futureShowListings.length > 1" class="relative flex py-4 md:py-6 overflow-x-hidden text-xl md:text-4xl bg-slate-900 rotate-[-0.5deg]"  @mouseover="paused = true" @mouseleave="paused = false">
+        <div v-if="futureShowListings.length > 1" class="relative flex py-4 md:py-6 overflow-x-hidden text-xl md:text-4xl bg-slate-900 md:rotate-[-0.5deg]"  @mouseover="paused = true" @mouseleave="paused = false">
             <div class="text-white whitespace-nowrap animate-marquee flex" :style="paused ? 'animation-play-state: paused;' :''">
                 <div v-for="show in futureShowListings"><span class="mx-10 font-black uppercase">{{show.location}} <span class="font-thin">{{ $dayjs(show.eventDate).format('MMM D, YYYY')}}</span></span></div>
             </div>
@@ -18,12 +18,12 @@
                 <div v-for="show in futureShowListings"><span class="mx-10 font-black uppercase">{{show.location}} <span class="font-thin">{{ $dayjs(show.eventDate).format('MMM D, YYYY')}}</span></span></div>
             </div>
         </div>
-        <div v-else-if="futureShowListings.length == 1" class="relative flex py-4 md:py-6 overflow-x-hidden text-xl md:text-4xl bg-slate-900 rotate-[-0.5deg] items-center justify-center">
+        <div v-else-if="futureShowListings.length == 1" class="relative flex py-4 md:py-6 overflow-x-hidden text-xl md:text-4xl bg-slate-900 md:rotate-[-0.5deg] items-center justify-center">
             <div class="text-white whitespace-nowrap flex">
                 <div v-for="show in futureShowListings"><span class="mx-10 font-black uppercase">{{show.location}} <span class="font-thin">{{ $dayjs(show.eventDate).format('MMM D, YYYY')}}</span></span></div>
             </div>
         </div>
-        <div v-else class="relative flex py-4 md:py-6 overflow-x-hidden text-xl md:text-4xl bg-slate-900 rotate-[-0.5deg] items-center justify-center">
+        <div v-else class="relative flex py-4 md:py-6 overflow-x-hidden text-xl md:text-4xl bg-slate-900 md:rotate-[-0.5deg] items-center justify-center">
             <div class="text-white whitespace-nowrap flex">
                 <div><span class="mx-10 font-bold uppercase">No Upcoming Shows</span></div>
             </div>
