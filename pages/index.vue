ecbbdeb16853048f6e08bc36ad9ecd2f6d9d9577
Greg Bates <gbatesy@gmail.com>
2023-10-18 20:35:11 -0300

    Resize TMC logo mobile

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@
                     <div class="absolute inset-0 z-20 bg-gradient-to-r from-red-500 to-yellow-500 opacity-50"></div>
                     <div class="absolute inset-0 z-30 flex flex-col">
                         <div class="absolute inset-0 bg-gradient-to-br from-black via-transparent to-transparent"></div>
-                        <nuxt-img provider="cloudflare" src="1b473f4e-e736-4e7c-d024-2c7118740100/w=550" class="w-72 rotate-[-3deg] z-10 p-4" />
+                        <nuxt-img provider="cloudflare" src="1b473f4e-e736-4e7c-d024-2c7118740100/w=550" class="w-36 md:w-72 rotate-[-3deg] z-10 p-4" />
                         <div class="absolute bottom-0 left-0 right-0 z-40 flex justify-center items-end">
                             <div class="bg-gradient-to-t from-black to-transparent py-6 px-4 w-full">
                                 <h2 class="text-white text-lg mb-2"><span class="font-bold">Latest Stream:</span> <span class="font-light">4 days ago</span></h2>
