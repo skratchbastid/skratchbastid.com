eaf1ca7bd54b820ccf87182b30699491488f5138
Greg Bates <gbatesy@gmail.com>
2023-10-21 23:28:30 -0300

    Improve mobile dates styling

--- pages/shows/index.vue
+++ pages/shows/index.vue
@@ -18,21 +18,21 @@ onMounted(() => {
     <div class="bg-[url(https://imagedelivery.net/pBWn_5oX0Np5kM_4V8gAww/99af408d-6c49-4743-41f2-9e14cfaeb500/w=1920)] bg-cover bg-no-repeat w-full min-h-[95vh]">
         <div class="flex flex-col w-full md:w-[60%] m-auto pt-16 gap-x-4">
             <h1 class="uppercase text-center text-5xl font-black text-shadow text-white">Upcoming Shows</h1>
-            <div class="my-8">
+            <div class="my-8 px-2">
                 <div v-for="show in futureShowListings" class="flex w-full items-center my-4 bg-white px-6 py-4 border-2 border-black rounded">
                     <div class="leading-tight w-1/10 text-right">
                         <div class="uppercase text-sm font-bold leading-none">{{ $dayjs(show.eventDate).format('MMM')}}</div>
                         <div class="text-right text-sm md:text-2xl font-bold leading-none">{{ $dayjs(show.eventDate).format('D')}}</div>
                     </div>
-                    <div class="flex flex-col md:flex-row flex-grow md:flex-grow-0 w-9/12 items-center justify-center">
-                        <div class="text-lg font-black uppercase md:w-2/3 md:text-2xl text-center">{{ show.location }}</div>
+                    <div class="flex flex-col lg:flex-row flex-grow md:flex-grow-0 w-9/12 items-center justify-center">
+                        <div class="font-black uppercase md:w-2/3 md:text-2xl text-center">{{ show.location }}</div>
                         <div class="md:w-1/2 text-center">
                             <div class="text-sm leading-none md:text-lg md:leading-none uppercase mb-1">{{ show.venue }}</div>
                             <div class="text-xs text-blue-600 font-bold uppercase" v-show="show.details">{{ show.details }}</div>
                         </div>
                     </div>
                     <div class="ml-auto w-3/10">
-                        <a v-if="show.ticketLink" :href="show.ticketLink" target="_blank" class="uppercase bg-teal-600 text-white py-3 px-5 rounded font-bold">Tickets</a>
+                        <a v-if="show.ticketLink" :href="show.ticketLink" target="_blank" class="uppercase bg-teal-600 text-white text-xs md:text-base px-3 py-3 md:px-5 rounded font-bold">Tickets</a>
                     </div>
                 </div>
             </div>
