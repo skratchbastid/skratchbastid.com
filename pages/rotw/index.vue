54d20caaa2b73dc637f3d6ea39c6702d921b912d
Greg Bates <gbatesy@gmail.com>
2023-10-23 08:50:45 -0300

    Tweak padding on ROTW index

--- pages/rotw/index.vue
+++ pages/rotw/index.vue
@@ -9,10 +9,10 @@
 </script>
 
 <template>
-    <div class="py-10" style="background: linear-gradient(135deg, rgba(0,0,0,0) 0%, rgba(195,207,226,0.5) 100%);">
+    <div class="py-5 md:py-10" style="background: linear-gradient(135deg, rgba(0,0,0,0) 0%, rgba(195,207,226,0.5) 100%);">
         <div class="px-4">
             <nuxt-img provider="cloudflare" src="ca8fd383-4e18-4141-555c-221ae8853d00/w=500" alt="" class="w-1/2 max-w-[300px] m-auto" />
-            <nuxt-link to="/newsletter" class="block bg-[#C81C2F] rounded-lg p-2 lg:p-4 text-white text-sm lg:text-base text-center my-8 max-w-[1200px] m-auto lg:leading-none hover:bg-[#A6192B] transition-colors duration-200">
+            <nuxt-link to="/newsletter" class="block bg-[#C81C2F] rounded-lg p-2 lg:p-4 my-5 text-white text-sm lg:text-base text-center max-w-[1200px] m-auto lg:leading-none hover:bg-[#A6192B] transition-colors duration-200">
                 <span class="font-bold block flex items-center justify-center">SUBSCRIBE<Icon name="material-symbols:chevron-right-rounded" size="22" /></span>
                 <span class="text-xs">Get the ROTW in your inbox every week!</span>
             </nuxt-link>
