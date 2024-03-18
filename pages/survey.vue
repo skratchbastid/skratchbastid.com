ce7f9d33332189bc34fccae23f281fbfdcff3ad6
Greg Bates <gbatesy@gmail.com>
2024-03-18 12:57:39 -0300

    Top Grillin Survey

---
+++ pages/survey.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const script = document.createElement('script');
+  script.src = '//embed.typeform.com/next/embed.js';
+  document.body.appendChild(script);
+});
+</script>
+
+<template>
+  <div data-tf-live="01HS940XVMGVFDP0PTN8EGPT01"></div>
+</template>
