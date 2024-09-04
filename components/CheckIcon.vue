e758a67f5487e4d0bbfdf2444609b6d25640455c
Greg Bates <gbatesy@gmail.com>
2024-09-04 14:30:11 -0300

    Icons fix

---
+++ components/CheckIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <Icon 
+    :name="outlined ? 'heroicons:check-circle-outline' : 'heroicons:check-circle'" 
+    :class="[color, size]"
+  />
+</template>
+
+<script setup>
+defineProps({
+  outlined: {
+    type: Boolean,
+    default: false
+  },
+  color: {
+    type: String,
+    default: 'text-green-500'
+  },
+  size: {
+    type: String,
+    default: 'w-6 h-6'
+  }
+})
+</script>
