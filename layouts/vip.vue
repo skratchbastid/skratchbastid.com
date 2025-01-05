530352fc47df405e807003f1259fcf4f41cd16e8
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2025-01-05 19:29:26 +0100

    Changes

--- layouts/vip.vue
+++ layouts/vip.vue
@@ -143,7 +143,7 @@
         <client-only>
           <div v-if="isClient && user?.id" class="relative flex justify-center items-center">
             <img v-if="user?.avatar" :src="user?.avatar?.url" class="w-8 rounded-full cursor-pointer" @click="toggleMenu" />
-            <div v-if="menuVisible" class="absolute block bg-white w-[200px] border border-1 m-auto top-[40px] text-center py-2">
+            <div v-if="menuVisible" class="absolute block bg-white w-[200px] border border-1 m-auto top-[40px] text-center py-2 r-0">
               <ul class="flex flex-col gap-y-2">
                 <li>
                   <a href="https://wp.skratchbastid.com/account" target="_blank" @click="menuVisible = false">My Account</a>
