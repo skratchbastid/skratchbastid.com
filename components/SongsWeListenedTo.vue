212ca07c565fa5c525b14ed2200f3c65df94d750
johnny-djjazzyjeff <johnny@djjazzyjeff.com>
2026-01-14 21:13:00 +0700

    Fix URL

--- components/SongsWeListenedTo.vue
+++ components/SongsWeListenedTo.vue
@@ -7,7 +7,7 @@
             <h3 class="mb-2 border-teal-600 border-2 text-teal-600 font-bold uppercase text-xs px-2 md:px-4 py-1 rounded">New Mix!</h3>
             <h2 class="mb-2 md:mb-6 uppercase font-black text-lg md:text-5xl leading-tight">Songs We Listened To A Lot In 2023</h2>
             <p class="mb-1 md:mb-2 text-center md:text-left md:text-lg">Bastid and Cosmo Baker are back with another bangin' year end mix chalk full of amazing musical discoveries new and old.</p>
-            <NuxtLink to="/mixes/songs-we-listened-to-alot-in-2023" class="mt-4 md:my-8 bg-blue-600 text-white text-center font-black px-8 md:w-auto md:px-12 py-2 rounded">LISTEN NOW</NuxtLink>
+            <NuxtLink to="https://www.mixcloud.com/skratchbastid/skratch-bastid-cosmo-baker-songs-we-listened-to-a-lot-in-2023/" class="mt-4 md:my-8 bg-blue-600 text-white text-center font-black px-8 md:w-auto md:px-12 py-2 rounded">LISTEN NOW</NuxtLink>
         </div>
     </section>
 </template>
